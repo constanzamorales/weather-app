6d032d396ae12ae5518a5d7494070f868a321c90
constanzamorales <constanzabmorales@gmagmail.com>
2021-03-09 12:45:19 -0300

    Added searchForm function to display name of the city the user submitted

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ let time = currentDate.toLocaleTimeString(navigator.language, {hour: '2-digit', 
 let today = document.querySelector('#today');
 today.innerHTML = `${day[currentDate.getDay()]} ${time}`;
 
-console.log(`${day[currentDate.getDay()]} ${time}`);
+function searchForm(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = `${searchInput.value}`;
+}
+
+let searchButton = document.querySelector("#search-form");
+searchButton.addEventListener("submit", searchForm);
