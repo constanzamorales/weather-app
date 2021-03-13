00082cc53a77c8ef985c17c7b998d41b70bd0c28
constanzamorales <constanzabmorales@gmail.com>
2021-03-13 16:32:59 -0300

    Implemented the unit converter, forecast display and changed colours

--- src/index.js
+++ src/index.js
@@ -1,96 +1,117 @@
-// Searching for city
-function searchForm(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
+// Getting date
+function getCurrentDate(timestamp) {
+  let currentDate = new Date();
+  let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = week[currentDate.getDay()];
+  return `${day} ${formatTime(timestamp)}`;
+}
+
+// Formatting current time
+function formatTime(timestamp) {
+  let currentDate = new Date(timestamp);
+  let hours = currentDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+// Searching for the city in the API
+function searchCity(city) {
   let apiKey = "2de1e0b71614dec5ecd1e018c409e23c";
   let units = "metric";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=${units}`; 
-  city.innerHTML = `${searchInput.value}`;
-  axios.get(url).then(function retreiveCityName(city) {
-    console.log(city.data);
-    let cityName = city.data.name;
-    let countryName = city.data.sys.country;
-    let citySearched = document.querySelector("#city");
-    citySearched.innerHTML = `${cityName}, ${countryName}`;  
-    axios.get(url).then(displayWeather);
-  });
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(url).then(displayWeather);
+  url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(url).then(displayForecast);
+}
+
+// Handling submit city
+function submitCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  searchCity(searchInput.value);
 }
 
 // Displaying current day's weather
 function displayWeather(response) {
-  let temp = Math.round(response.data.main.temp);
+  let cityName = response.data.name;
+  let countryName = response.data.sys.country;
+  let citySearched = document.querySelector("#city");
+  citySearched.innerHTML = `${cityName}, ${countryName}`;
+
+  let tempCelsius = Math.round(response.data.main.temp);
   let weatherCondition = response.data.weather[0].description;
   let pressure = (response.data.main.pressure).toLocaleString(undefined, {minimunFractionDigits: 0});
   let humidity = response.data.main.humidity;
   let wind = response.data.wind.speed;
-
-  let conditionId = response.data.weather[0].id;
-  let conditionIdArray = (""+conditionId).split('').map(Number);
   let iconElement = document.querySelector("#icon");
 
-  console.log(conditionIdArray);
-
-  document.querySelector("#temperature").innerHTML = `${temp}`;
-  document.querySelector("#weather-condition").innerHTML = weatherCondition.charAt(0).toUpperCase() + weatherCondition.slice(1);
+  document.querySelector("#temperature").innerHTML = `${tempCelsius}`;
+  document.querySelector("#current-weather-condition").innerHTML = weatherCondition.charAt(0).toUpperCase() + weatherCondition.slice(1);
   document.querySelector("#pressure").innerHTML = `<i class="pressure-icon"></i>${pressure} hPa`;
   document.querySelector("#humidity").innerHTML = `<i class="humidity-icon"></i>${humidity}%`;
   document.querySelector("#wind").innerHTML = `<i class="wind-icon"></i>${wind} km/h`;
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
 
-  
-  if (conditionIdArray[0] === 2) {
-    iconElement.setAttribute("src", 'https://gist.githubusercontent.com/constanzamorales/90e126802b1bc12e207dab13cee01f3b/raw/af8c49e7c926373d6c651acb5f58545ec988b2e4/thunderstorm.svg');
-  } else 
-  if (conditionIdArray[0] === 3) {
-    iconElement.setAttribute("src", 'https://gist.githubusercontent.com/constanzamorales/90e126802b1bc12e207dab13cee01f3b/raw/af8c49e7c926373d6c651acb5f58545ec988b2e4/shower-rain.svg');
-  } else
-  if (conditionIdArray[0] === 5) {
-    iconElement.setAttribute("src", 'https://gist.githubusercontent.com/constanzamorales/90e126802b1bc12e207dab13cee01f3b/raw/af8c49e7c926373d6c651acb5f58545ec988b2e4/rain.svg');
-  } else
-  if (conditionIdArray[0] === 6) {
-    iconElement.setAttribute("src", 'https://gist.githubusercontent.com/constanzamorales/90e126802b1bc12e207dab13cee01f3b/raw/af8c49e7c926373d6c651acb5f58545ec988b2e4/snow.svg');
-  } else
-  if (conditionIdArray[0] === 7) {
-    iconElement.setAttribute("src", 'https://gist.githubusercontent.com/constanzamorales/90e126802b1bc12e207dab13cee01f3b/raw/af8c49e7c926373d6c651acb5f58545ec988b2e4/mist.svg');
-  } else
-  if (conditionIdArray[0] === 8 && conditionIdArray[2] === 1 || conditionIdArray[0] === 8 && conditionIdArray[2] === 2) {
-    iconElement.setAttribute("src", 'https://gist.githubusercontent.com/constanzamorales/90e126802b1bc12e207dab13cee01f3b/raw/59b96aade2621c843e1e24b620c4aec292bfa815/scattered-clouds.svg');
-  } else
-  if (conditionIdArray[0] === 8 && conditionIdArray[2] === 3 || conditionIdArray[0] === 8 && conditionIdArray[2] === 4) {
-    iconElement.setAttribute("src", 'https://gist.githubusercontent.com/constanzamorales/90e126802b1bc12e207dab13cee01f3b/raw/59b96aade2621c843e1e24b620c4aec292bfa815/broken-clouds.svg');
-  } else
-  if (conditionIdArray[0] === 8 && conditionIdArray[2] === 0) {
-    iconElement.setAttribute("src", 'https://gist.githubusercontent.com/constanzamorales/90e126802b1bc12e207dab13cee01f3b/raw/af8c49e7c926373d6c651acb5f58545ec988b2e4/clear-sky.svg');
+function displayForecast(response) {
+  console.log(response.data.list);
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  let weatherCondition = null;
+
+  for (let index = 0; index < 5; index++) {
+    weatherCondition = response.data.list[index].weather[0].description;
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="card text-center">
+    <h5 class="card-title">
+      ${formatTime(forecast.dt * 1000)}
+    </h5>
+    <div class="card-body">
+    <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" class="card-img-top small-weather-icon">
+    <p id="weather-condition" class="card-text">
+      ${weatherCondition.charAt(0).toUpperCase() + weatherCondition.slice(1)} <br />
+    <strong>${Math.round(forecast.main.temp_max)}°</strong> / ${Math.round(forecast.main.temp_min)}°
+    </p>
+    </div>
+    </div>`
   }
 }
 
-// Getting current date
-let currentDate = new Date();
-let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let time = currentDate.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
-let today = document.querySelector("#today");
-today.innerHTML = `${day[currentDate.getDay()]} ${time}`;
-
-// Executing searchForm function when user clicks the search button
-let searchButton = document.querySelector("#search-form");
-searchButton.addEventListener("submit", searchForm);
-
-/*
-// Bonus feature
 function convertToFahrenheit(event) {
-  event.preventDefault
+  event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 81;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let tempFahrenheit = (tempCelsius * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(tempFahrenheit);
 }
 
 function convertToCelsius(event) {
-  event.preventDefault
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 27;
+  temperatureElement.innerHTML = Math.round(tempCelsius);
 }
 
+// Executing searchForm function when user clicks the search button
+let searchButton = document.querySelector("#search-form");
+searchButton.addEventListener("submit", submitCity);
+
+// Triggering the unit conversion
+let tempCelsius = null;
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
-*/
+
+searchCity("Santiago");
